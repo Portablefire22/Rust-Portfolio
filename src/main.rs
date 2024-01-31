fbe73c1048d1fbc458aa9eb5d8ac332f53eb9f74
Portablefire22 <barnowlsarecool@gmail.com>
2024-01-31 00:41:51 +0000

    Added the index page, started work on preview project page. Side note: FUCK HTML & CSS

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,39 @@
 #[macro_use]
 extern crate rocket;
 
+use rocket::fs::{FileServer, NamedFile};
+use rocket_dyn_templates::context;
+use rocket_dyn_templates::Template;
 #[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+fn index() -> Template {
+    Template::render("index", context! { foo: 123 })
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    // The "mount" string determines the path, the route is the final section of it
+    // eg build().mount("/projects")
+    // Would mount to 127.0.0.1:8000/projects/<route>
+    rocket::build()
+        .mount("/", routes![index])
+        .mount("/css/", routes![stylesheet])
+        .mount("/projects/", routes![projectIndex, projectKitty])
+        .mount("/public/", FileServer::from("public/"))
+        .attach(Template::fairing())
+}
+
+// ------------------------------------------ Project routes ------------------------------------------
+#[get("/")]
+async fn projectIndex() -> Template {
+    Template::render("projects/kitty", context! {})
+}
+
+#[get("/KittyGoesMeow")]
+async fn projectKitty() -> Template {
+    Template::render("projects/kitty", context! {})
+}
+
+#[get("/style.css")]
+async fn stylesheet() -> Option<NamedFile> {
+    NamedFile::open("templates/css/style.css").await.ok()
 }
